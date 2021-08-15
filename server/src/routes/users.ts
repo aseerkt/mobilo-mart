2332c45f676a6e78cf099be835090d20ab1bdd6d
Aseer KT <aseermiqdad@gmail.com>
2021-08-15 17:09:16 +0530

    api: added me controller to user route

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { login, register } from '../controllers/userController';
+import { login, me, register } from '../controllers/userController';
+import { checkAuth } from '../middlewares/permissions';
 
 const router = Router();
 
 router.post('/', register);
 router.post('/login', login);
+router.get('/', checkAuth, me);
 
 export default router;
