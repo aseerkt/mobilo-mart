1d2b971e062890e1d312dcffb5e876460c1c5650
Aseer KT <aseermiqdad@gmail.com>
2021-09-21 19:05:34 +0530

    feat: order item db entity

---
+++ server/src/entities/OrderItem.ts
@@ -0,0 +1,31 @@
+import { Entity, Enum, ManyToOne, PrimaryKey, Property } from '@mikro-orm/core';
+import { v4 } from 'uuid';
+import User from './User';
+import Mobile from './Mobile';
+
+export enum OrderItemStatus {
+  CANCELLED = 'cancelled',
+  PAID = 'paid',
+  DELIVERED = 'delivered',
+}
+
+@Entity({ tableName: 'order-items' })
+export default class OrderItem {
+  @PrimaryKey()
+  id: string = v4();
+
+  @Property()
+  qty: number;
+
+  @ManyToOne(() => User)
+  user: User;
+
+  @ManyToOne(() => Mobile)
+  Mobile: Mobile;
+
+  @Enum({
+    type: 'OrderItemStatus',
+    items: () => OrderItemStatus,
+  })
+  status: OrderItemStatus;
+}
