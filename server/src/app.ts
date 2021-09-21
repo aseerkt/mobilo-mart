08320868e54552ac0cf860671fc446efa4ba21e3
Aseer KT <aseermiqdad@gmail.com>
2021-09-21 19:03:52 +0530

    chore: import entity manager type from postgresql

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,31 +2,30 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
-import {
-  MikroORM,
-  EntityManager,
-  EntityRepository,
-  RequestContext,
-} from '@mikro-orm/core';
+import { MikroORM, EntityRepository, RequestContext } from '@mikro-orm/core';
 // import config from './mikro-orm.config';
 import User from './entities/User';
 import Mobile from './entities/Mobile';
 
 import userRoutes from './routes/users';
 import productRoutes from './routes/products';
+import { EntityManager } from '@mikro-orm/postgresql';
+import OrderItem from './entities/OrderItem';
 
 export const DI = {} as {
   orm: MikroORM;
   em: EntityManager;
   userRepository: EntityRepository<User>;
   mobileRepository: EntityRepository<Mobile>;
+  orderRepository: EntityRepository<OrderItem>;
 };
 
 export default async function createApp() {
   DI.orm = await MikroORM.init();
-  DI.em = DI.orm.em;
+  DI.em = DI.orm.em as EntityManager;
   DI.userRepository = DI.orm.em.getRepository(User);
   DI.mobileRepository = DI.orm.em.getRepository(Mobile);
+  DI.orderRepository = DI.orm.em.getRepository(OrderItem);
 
   const app = express();
 
