a867032441c32b3a84549cd421be6975f51c5b37
Aseer KT <aseermiqdad@gmail.com>
2021-10-15 09:09:41 +0530

    feat: nprogress on route change

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider, GlobalStyle } from '@chakra-ui/react';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 import '@fontsource/montserrat/400.css';
 import '@fontsource/montserrat/500.css';
 import '@fontsource/montserrat/700.css';
@@ -16,6 +19,10 @@ function ShowNavbar() {
 
   return <>{showNavbar && <Navbar />}</>;
 }
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { initialState } = pageProps;
