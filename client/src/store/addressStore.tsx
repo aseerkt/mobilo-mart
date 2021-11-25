d36a6f5265eb7e3d2e18992527fe12715b667796
Aseer KT <aseermiqdad@gmail.com>
2021-11-25 09:43:16 +0530

    chore: zustand context

--- client/src/store/addressStore.tsx
+++ client/src/store/addressStore.tsx
@@ -1,9 +1,8 @@
-import create from 'zustand';
-import { persist } from 'zustand/middleware';
 import { v4 } from 'uuid';
 import { Address } from '@/types/address';
+import { StoreSlice } from '../types/store';
 
-export interface AddressStore {
+export interface AddressSlice {
   currentAddressId?: string;
   addresses: Address[];
   hasCurrentAddress: () => boolean;
@@ -14,35 +13,30 @@ export interface AddressStore {
   selectAddress: (id: string) => void;
 }
 
-const useAddressStore = create<AddressStore>(
-  persist(
-    (set, get) => ({
-      addresses: [],
-      addAddress: (address) => {
-        return set({
-          addresses: get().addresses.concat({
-            id: v4(),
-            ...address,
-          } as Address),
-        });
-      },
-      editAddress: (address) =>
-        set({
-          addresses: get()
-            .addresses.filter((a) => a.id !== address.id)
-            .concat(address as Address),
-        }),
-      hasCurrentAddress: () =>
-        get().addresses.some((a) => a.id === get().currentAddressId),
-      getCurrentAddress: () =>
-        get().addresses.find((a) => a.id === get().currentAddressId),
-      removeAddress: (id) =>
-        set({ addresses: get().addresses.filter((a) => a.id !== id) }),
-      selectAddress: (id) =>
-        set({ currentAddressId: get().addresses.find((a) => a.id === id)?.id }),
+const useAddressStore: StoreSlice<AddressSlice> = (set, get) => ({
+  addresses: [],
+  addAddress: (address) => {
+    return set({
+      addresses: get().addresses.concat({
+        id: v4(),
+        ...address,
+      } as Address),
+    });
+  },
+  editAddress: (address) =>
+    set({
+      addresses: get()
+        .addresses.filter((a) => a.id !== address.id)
+        .concat(address as Address),
     }),
-    { name: 'address_data' }
-  )
-);
+  hasCurrentAddress: () =>
+    get().addresses.some((a) => a.id === get().currentAddressId),
+  getCurrentAddress: () =>
+    get().addresses.find((a) => a.id === get().currentAddressId),
+  removeAddress: (id) =>
+    set({ addresses: get().addresses.filter((a) => a.id !== id) }),
+  selectAddress: (id) =>
+    set({ currentAddressId: get().addresses.find((a) => a.id === id)?.id }),
+});
 
 export default useAddressStore;
--- client/src/store/cartStore.tsx
+++ client/src/store/cartStore.tsx
@@ -1,11 +1,10 @@
 import { Mobile } from '@/types/mobile';
-import create from 'zustand';
-import { persist } from 'zustand/middleware';
 import { OrderItem } from '../types/order';
+import { StoreSlice } from '../types/store';
 
 export type CartItemType = Omit<OrderItem, 'status' | 'createdAt' | 'id'>;
 
-interface CartStore {
+export interface CartSlice {
   cartItems: CartItemType[];
   getTotalPrice: () => number;
   getTotalQty: () => number;
@@ -16,62 +15,46 @@ interface CartStore {
   clearCart: () => void;
 }
 
-const useCartStore = create<CartStore>(
-  persist(
-    (set, get) => ({
-      cartItems: [],
-      addToCart: (mobile, qty) => {
-        const itemIdx = get().cartItems.findIndex(
-          (i) => i.mobile.id === mobile.id
-        );
-        if (itemIdx !== -1) {
-          get().changeItemQty(mobile.id, qty);
-        } else {
-          return set({
-            cartItems: [...get().cartItems, { mobile, qty }],
-          });
-        }
-      },
-      getTotalPrice: () =>
-        get().cartItems.reduce(
-          (prev, curr) => prev + curr.qty * curr.mobile.price,
-          0
-        ),
-      getTotalQty: () =>
-        get().cartItems.reduce((prev, curr) => prev + curr.qty, 0),
-      isCartEmpty: () => get().cartItems.length === 0,
-      removeItem: (mobileId) => {
-        const { cartItems } = get();
-        const newItems = cartItems.filter((i) => i.mobile.id !== mobileId);
-        return set({ cartItems: newItems });
-      },
-      changeItemQty: (mobileId, qty) => {
-        if (qty === 0) {
-          get().removeItem(mobileId);
-        } else {
-          const itemIdx = get().cartItems.findIndex(
-            (i) => i.mobile.id === mobileId
-          );
-          const newItems = get().cartItems.filter(
-            (i) => i.mobile.id !== mobileId
-          );
-          newItems.splice(itemIdx, 0, {
-            ...get().cartItems[itemIdx],
-            qty,
-          });
-          return set({ cartItems: newItems });
-        }
-      },
-      clearCart: () => set({ cartItems: [] }),
-    }),
-    { name: 'cart_data' }
-  )
-);
+const cartSlice: StoreSlice<CartSlice> = (set, get) => ({
+  cartItems: [],
+  addToCart: (mobile, qty) => {
+    const itemIdx = get().cartItems.findIndex((i) => i.mobile.id === mobile.id);
+    if (itemIdx !== -1) {
+      get().changeItemQty(mobile.id, qty);
+    } else {
+      return set({
+        cartItems: [...get().cartItems, { mobile, qty }],
+      });
+    }
+  },
+  getTotalPrice: () =>
+    get().cartItems.reduce(
+      (prev, curr) => prev + curr.qty * curr.mobile.price,
+      0
+    ),
+  getTotalQty: () => get().cartItems.reduce((prev, curr) => prev + curr.qty, 0),
+  isCartEmpty: () => get().cartItems.length === 0,
+  removeItem: (mobileId) => {
+    const { cartItems } = get();
+    const newItems = cartItems.filter((i) => i.mobile.id !== mobileId);
+    return set({ cartItems: newItems });
+  },
+  changeItemQty: (mobileId, qty) => {
+    if (qty === 0) {
+      get().removeItem(mobileId);
+    } else {
+      const itemIdx = get().cartItems.findIndex(
+        (i) => i.mobile.id === mobileId
+      );
+      const newItems = get().cartItems.filter((i) => i.mobile.id !== mobileId);
+      newItems.splice(itemIdx, 0, {
+        ...get().cartItems[itemIdx],
+        qty,
+      });
+      return set({ cartItems: newItems });
+    }
+  },
+  clearCart: () => set({ cartItems: [] }),
+});
 
-export const hydrateStore = (initialState) => {
-  if (initialState) {
-    useCartStore.setState(initialState);
-  }
-};
-
-export default useCartStore;
+export default cartSlice;
